e7506efcc00b63ec4b527f1d6089f480ddd92aa5
Shortcourse <shortcourse@Shortcourses-iMac-10.local>
2019-01-27 13:18:13 +0800

    My first java program

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+	/**
+	My First Java Program
+	*/
+	public static void main (String[] args){
+		//prints the string “Hello world” on screen
+		System.out.println("Hello world");
+
+	}
+}
